34d75ec11753ca9eab9803f516951fe91a455193
jun <junpeeei0407@gmail.com>
2022-12-21 01:07:23 +0900

    ブラウザ非表示

--- pj_pic_share20_api/settings.py
+++ pj_pic_share20_api/settings.py
@@ -123,11 +123,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
-# REST_FRAMEWORK = { 
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     ),
-# }
+REST_FRAMEWORK = { 
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
 
 
 # Internationalization
